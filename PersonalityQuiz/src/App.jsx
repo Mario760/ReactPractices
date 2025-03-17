85153480518f2d4302fbf33ae4c92e2c877cac2e
JIAWEI MA <jiaweima@cmail.carleton.ca>
2025-03-17 00:13:16 -0400

    initialize the PersonalityQuiz project

---
+++ PersonalityQuiz/src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import { UserProvider } from './components/UserContext';
+import Header from './components/Header';
+import Question from "./components/Question";
+import Results from "./components/Results";
+import UserForm from './components/UserForm';
+import {Routes, Route} from "react-router-dom";
+
+function App() {
+  const questions = [
+    {
+      question: "What's your favorite color?",
+      options: ["Red 🔴", "Blue 🔵", "Green 🟢", "Yellow 🟡"],
+    },
+  ];
+
+  const keywords = {
+    Fire: "fire",
+    Water: "water",
+    Earth: "earth",
+    Air: "air",
+  };
+
+  const elements = {
+    "Red 🔴": "Fire",
+    "Blue 🔵": "Water",
+    "Green 🟢": "Earth",
+    "Yellow 🟡": "Air",
+    // Continue mapping all your possible options to a keyword
+  };
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [element, setElement] = useState("");
+  const [artwork, setArtwork] = useState(null);
+
+  function handleAnswer(answer) {
+    setAnswers([...answers, answer]);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+  
+  function handleUserFormSubmit(name) {
+    setUserName(name);
+  };
+  
+  function determineElement(answers) {
+    const counts = {};
+    answers.forEach(function(answer) {
+      const element = elements[answer];
+      counts[element] = (counts[element] || 0) + 1;
+    });
+    return Object.keys(counts).reduce(function(a, b) {
+      return counts[a] > counts[b] ? a : b
+    });
+  };
+
+  useEffect(
+    function () {
+      if (currentQuestionIndex === questions.length) {
+        const selectedElement = determineElement(answers);
+        setElement(selectedElement);
+        fetchArtwork(keywords[selectedElement]);
+      }
+    },
+    [currentQuestionIndex]
+  );
+
+  return (
+    <div>
+      <UserProvider value={{name:userName, setName:setUserName}} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<UserForm onSubmit={handleUserFormSubmit} />} />
+        <Route
+          path="/quiz"
+          element={
+            currentQuestionIndex < questions.length ? (
+              <Question question={questions[currentQuestionIndex].question} options={questions[currentQuestionIndex].options} onAnswer={handleAnswer} />
+            ) : (
+              <Results element={element} artwork={artwork} />
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
---
+++ PersonalityQuiz/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+export default function Header(){
+    return (<div>
+        <h1>Which Element Are You?</h1><br />
+        <p>based on completely random things</p> <br />
+        <Link to="/">Home</Link>
+        <Link to='/quiz'>Quiz</Link>
+    </div>);
+}
